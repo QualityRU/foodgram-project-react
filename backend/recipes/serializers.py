fd7ad30c02a744bbf3b1b7cf9d4134c49ecef32e
QualityRU <94740104+QualityRU@users.noreply.github.com>
2023-09-08 16:22:01 +0300

    Update serializers.py

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -7,6 +7,7 @@ from .validators import ColorFieldValidator
 from users.serializers import UserListSerializer
 
 
+
 class TagSerializer(serializers.ModelSerializer):
     """Сериализатор для тегов."""
 
@@ -53,11 +54,19 @@ class RecipeListSerializer(serializers.ModelSerializer):
         )
 
     def get_is_favorited(self, recipe):
+        if not self.context.get('request'):
+            return False
         user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
         return recipe.favorite.filter(user=user).exists()
 
     def get_is_in_shopping_cart(self, recipe):
+        if not self.context.get('request'):
+            return False
         user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
         return recipe.shoppingcart.filter(user=user).exists()
 
 
